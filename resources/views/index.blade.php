e39272849cd98153a9277d95553e7e8be4127c3d
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-12 20:29:07 +0100

    Second..maybe, I am not sure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index Blade</title>
-</head>
-<body>
+@extends('layouts.master')
 
+@section('content')
+
+@foreach ($movies as $movie)
+<h1><a href="{{ 'movies/' . $movie->id }} "> {{ $movie->title }} </a></h1>
+<p> {{ $movie->storyline }} </p>
+
+@endforeach
     
-
-        @foreach ($movies as $movie)
-            <h1><a href="{{ 'movies/' . $movie->id }} "> {{ $movie->title }} </a></h1>
-            <p> {{ $movie->storyline }} </p>
-
-        @endforeach
+@endsection
 
 
-    
 
-    
-</body>
-</html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show blade</title>
-</head>
-<body>
+@extends('layouts.master')
 
-    {{ $movie }}
+@section('content')
     
-</body>
-</html>
+{{ $movie->title }}
+<br>
+{{ $movie->genre }}
+<br>
+{{ $movie->director }}
+<br>
+{{ $movie->year }}
+<br>
+{{ $movie->storyline }}
+
+
+@endsection
+
+
